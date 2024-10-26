7312e0d863373e26d3cca8f49b885ee84d0e689d
savana64 <xenawarrior@centrum.cz>
2024-10-26 21:20:57 +0200

    přidány pohledy do views

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,9 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
+def detail(request, question_id):
+    return HttpResponse("Koukáš na dotaz %s." % question_id)
 
 def index(request):
     return HttpResponse("Hello, world. You're at the police roblox.")
+
+def results(request, question_id):
+    response = "Koukáš na odpovědi na otázku %s."
+    return HttpResponse( response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Hlasuješ k dotazu %s." % question_id)
+
+
 
 # Create your views here.
