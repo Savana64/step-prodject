162f54eb6aa81f51e9e597ee6689f1b9d15d5724
savana64 <xenawarrior@centrum.cz>
2024-10-27 12:15:58 +0100

    doplnění pohledů, vytvoření index.html, úprava urlconf, odkaz na index v pohledu

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,11 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
+from .models import Question
+from .models import Choice
 
 def detail(request, question_id):
     return HttpResponse("Koukáš na dotaz %s." % question_id)
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the police roblox.")
+    latest_question_list = Question.objects.order_by('-pub_date') [:5]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list,}
+    return HttpResponse(template.render(context, request))
 
 def results(request, question_id):
     response = "Koukáš na odpovědi na otázku %s."
